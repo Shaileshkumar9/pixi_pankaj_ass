9ca2cf70691d961c2ba0e42b1f4b27a1315e2514
Shaileshkumar9 <shailesh.kumar_cs19@gla.ac.in>
2022-12-26 16:26:25 +0530

    new

--- PIXI_PANKAJ4/main.js
+++ PIXI_PANKAJ4/main.js
@@ -65,10 +65,7 @@ function loadAssets(list, onLoadComplete) {
 }
 //----------------------------------
 function start(loader, resources) {
-    //console.log('params ', arguments);
-  //  const back = PIXI.Sprite.from(resources['back'].texture);
-   // back.scale.set(0.2);
-   // game.stage.addChild(back);
+   
     const smily = new PIXI.Texture(resources['front'].texture);
     //new PIXI.Rectangle(0,0,150,150));
     const front = PIXI.Sprite.from(smily);
@@ -78,28 +75,7 @@ function start(loader, resources) {
     game.stage.addChild(front);
 
 
-//     const smily2 = new PIXI.Texture(resources['back'].texture);
-//     //new PIXI.Rectangle(0,0,150,150));
-//     const front2 = PIXI.Sprite.from(smily2);
-//      front2.scale.set(.6);
-//      front2.x=744;
-//      front2.width=100;
-//      front2.height=80;
-//     front2.y=272;
-//     game.stage.addChild(front2);
-//     front2.interactive = true;
-// front2.on('mousedown', (event) => {
 
-//      console.log("dbjhdsghj");
-//      document.getElementById("loader").style.display = "";
-//     // Handle event
-// });
-
-    
-   //var sprite = PIXI.Sprite.fromImage('');
-  // sprite.position.x = 100;
-  // sprite.position.y = 100;
-  // stage.addChi
       
     var text = new PIXI.Text('Welcome To our Game',
     {
